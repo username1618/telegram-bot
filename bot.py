18acfaea8953cd6d68e4bf28e64dfcd08b10301f
Vainu <Vainu@mail.ru>
2020-02-12 01:05:05 +0300

    initial commit bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+import telebot
+import parser
+
+#main variables
+TOKEN = "1090552842:AAGchKO-zHMRtVYj09QuuQwBVLC-glrEuAg"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду парсить заголовки с Хабра')
+
+@bot.message_handler(content_types=['photo'])
+def text_handler(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'Красиво.')
+
+bot.polling()
